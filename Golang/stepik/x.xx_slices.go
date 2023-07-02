c1c404ba88bb488adca97cd8e90375b0ff84c162
Олег Каминский <kamo@automacon.ru>
2023-07-02 17:19:05 +0300

    1.11_f_output

---
+++ Golang/stepik/x.xx_slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+    slice := buffer[10:20]
+    for i := 0; i < len(slice); i++ {
+        slice[i] = byte(i)
+    }
+    fmt.Println("before", slice)
+    AddOneToEachElement(slice)
+    fmt.Println("after", slice)
+}
+func AddOneToEachElement(slice []byte) {
+    for i := range slice {
+        slice[i]++
+    }
+}
